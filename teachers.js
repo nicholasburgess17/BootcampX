eeeaec39ae279b8b62f364a5b07d8308f9b770ee
Nicholas Burgess <105810519+nicholasburgess17@users.noreply.github.com>
2022-12-13 12:36:01 -0700

    Add files via upload

---
+++ teachers.js
@@ -0,0 +1,29 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+const queryString =`
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = '${cohortName || 'JUL02'}'
+ORDER BY teacher;
+`;
+
+
+pool.query(queryString, values)
+.then(res => {
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);
+  })
+});
